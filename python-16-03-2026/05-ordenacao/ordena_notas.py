c368e47bd6a1ec5b37727fcd7ae07c43687bb5a0
cleberleao <cleberleao_14@hotmail.com>
2026-03-16 21:14:02 -0300

    Ordenando lista

--- python-16-03-2026/05-ordenacao/ordena_notas.py
+++ python-16-03-2026/05-ordenacao/ordena_notas.py
@@ -2,3 +2,15 @@
 # 1. Ordene as notas em ordem crescente e exiba.
 # 2. Mostre maior, menor, média e mediana.
 # 3. Pergunte se deseja ordenar decrescente e exibir novamente.
+
+qtde = int(input("Quantas notas deseja entrar no sistema: "))
+notas = []
+
+for i in range(qtde):
+    notas.append(float(input("Digite a nota: ")))
+
+print("Notas na ordem de digitação", notas)
+notas.sort()
+print("Notas ordenadas crescente", notas)
+notas.sort(reverse = True)
+print("Notas ordenadas decrescente", notas)
